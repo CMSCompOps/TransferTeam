1edd736f8baa82b2543c66f7ca9d0713ca992412
Pradeep <35749353+pradeepjasal@users.noreply.github.com>
2020-01-29 13:41:21 -0600

    Delete aaa_federation.sh

--- AAAOps/AAA_Subscription/aaa_federation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-set -x 
-export PYTHONPATH=$PYTHONPATH:/root/pradeep/send_aaa_matrics/CMSMonitoring/src/python/
-python $PWD/test.py --amq /root/pradeep/send_aaa_matrics/credentials.json --data $PWD/aaa_schema.json 
