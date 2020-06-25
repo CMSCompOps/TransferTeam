09066a02b1e87b33bda973383e0924f05d10a527
Fernando Garzon <fernandogarzon@l-lml-127876.dhcp.fnal.gov>
2020-06-25 16:37:52 -0500

    Test_2

--- consistency_check/Check_dbs_invalidations_file.py
+++ consistency_check/Check_dbs_invalidations_file.py
@@ -12,7 +12,7 @@ def file_read(fname):
 def which_to_invalidate_in_dbs(fname):
     invalidate = []
     files = file_read(fname)
-    for line_ in range(len(files)):
+    for line_ in files:
         os.system("xrdfs cms-xrd-global.cern.ch locate -d -m "+line_+" > aux.txt")
         sites = file_read("aux.txt")
         if sites[0] == "[FATAL] Redirect limit has been reached":
