d73edf04d9b07bb2da939c2d4b1b8c9927764ce6
root <root@vocms036.cern.ch>
2023-10-11 02:19:11 +0200

    test update

--- AAAOps/FedProbeSendAAAMetrics/listSrv.sh
+++ AAAOps/FedProbeSendAAAMetrics/listSrv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ipv= ; ipos=2
 [ $# -gt 0 ] && { ipv=6 ; ipos=3 ; } ;
-
+[ -f out/xrdmapc_all_0.txt ] || exit 1
 for h in $( grep Srv out/xrdmapc_all_0.txt  | awk '{print $2}' | cut -d: -f1 | grep -v \\[ | sort -u ) ; do
 
    echo $h $(ping${ipv} -c 1 $h | grep ^PING | cut -d\( -f${ipos} | cut -d\) -f1)
